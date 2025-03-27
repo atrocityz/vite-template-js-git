a936383df51927c66137bb47e25c17c415db826b
atrocityz <disbalance17@Mail.ru>
2025-03-27 23:31:52 +0700

    added new prettier settings

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { globals: globals.browser } },
   { ignores: ['**/*.config.js'] },
   pluginJs.configs.recommended,
